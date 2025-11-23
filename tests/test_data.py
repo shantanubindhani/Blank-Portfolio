370a9d645c92b04b11a7a138c33aa1ec2e4c5f30
Shantanu Bindhani <48177913+shantanubindhani@users.noreply.github.com>
2025-11-23 11:06:49 +0530

    [UPDATE]: added tests for utils.py and workflow update (#4)
    * [NEW]: added tests for utils.py 
    
    * [UPDATE]: added commands to github workflow file!.

---
+++ tests/test_data.py
@@ -0,0 +1,13 @@
+import pytest 
+from typing import List
+from black_portfolio.data_models import Skill, SkillLevel, SkillType
+
+@pytest.fixture
+def sample_skills() -> List[Skill]:
+    return [
+        Skill(name="skill1", type=SkillType.LANGUAGE, level=SkillLevel.EXPERT),
+        Skill(name="skill2", type=SkillType.LANGUAGE, level=SkillLevel.ADVANCED),
+        Skill(name="skill3", type=SkillType.TOOL, level=SkillLevel.INTERMEDIATE),
+        Skill(name="skill4", type=SkillType.METHODOLOGY, level=SkillLevel.BEGINNER),
+        Skill(name="skill5", type=SkillType.TOOL, level=SkillLevel.EXPERT),
+    ]
